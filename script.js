10503b6e3c8e94190c97a35d53b11ecf7e00ce98
nadzeya <vitserresvit@gmail.com>
2023-10-14 22:43:17 +0300

    new

--- script.js
+++ script.js
@@ -7,36 +7,35 @@ let currentSlide = 0;
 circleBtn[0].style.opacity = "1";
 circleBtn[0].style.backgroundColor = "white";
 
-function updateSliderPosition() {
+function updateSliderPosition(slides, currentSlide) {
     const offset = `${-1 * currentSlide * 100}%`;
     for (let slide of slides) {
         slide.style.transform = `translateX(${offset})`;
     }
 }
 
-function updateCircleBtn() {
+function updateCircleBtn(circleBtn, currentSlide) {
     circleBtn.forEach(button => {
         button.style.opacity = "0.4"; 
-        button.style.backgroundColor = ""; 
     });
 
     circleBtn[currentSlide].style.opacity = "1"; 
-    circleBtn[currentSlide].style.backgroundColor = "white"; 
+     
 }
 
 function go_left() {
     if (currentSlide !== 0) {
         currentSlide--;
-        updateSliderPosition();
-        updateCircleBtn();
+        updateSliderPosition(slides, currentSlide);
+        updateCircleBtn(circleBtn, currentSlide);
     }
 }
 
 function go_right() {
     if (currentSlide < slides.length - 1) {
         currentSlide++;
-        updateSliderPosition();
-        updateCircleBtn();
+        updateSliderPosition(slides, currentSlide);
+        updateCircleBtn(circleBtn, currentSlide);
     }
 }
 
@@ -47,8 +46,8 @@ nextBtn.addEventListener('click', go_right);
 circleBtn.forEach((btn, ind) => {
     btn.addEventListener("click", () => {
         currentSlide = ind;
-        updateSliderPosition();
-        updateCircleBtn();
+        updateSliderPosition(slides, currentSlide);
+        updateCircleBtn(circleBtn, currentSlide);
     });
 });
 
@@ -64,44 +63,30 @@ let currentRoote = 0;
 mainThreeCicle[0].style.opacity = "1";
 
 
-function updateSliderPositionTwo() {
-    const offsetTwo = `${-1 * currentRoote * 1200}px`;
-    for(let slide of rooteImg){
-    slide.style.transform = `translateX(${offsetTwo})`;
-    } 
-}
-
 mainThreeLeft.addEventListener('click', () => {
     if (currentRoote > 0) {
         currentRoote--;
-        updateSliderPositionTwo();
-        updateCircles();
+        updateSliderPosition(rooteImg, currentRoote);
+        updateCircleBtn(mainThreeCicle, currentRoote)
     }
 });
 
 mainThreeRight.addEventListener('click', () => {
     if (currentRoote < rooteImg.length - 1) {
         currentRoote++;
-        updateSliderPositionTwo();
-        updateCircles();
+        updateSliderPosition(rooteImg, currentRoote);
+        updateCircleBtn(mainThreeCicle, currentRoote)
     }
 });
 
-function updateCircles() {
-    mainThreeCicle.forEach(circle => {
-        circle.style.opacity = "0.4";
-       
-    });
-    mainThreeCicle[currentRoote].style.opacity = "1";
-   
-}
+
 
 mainThreeCicle.forEach((btn, ind) => {
     btn.addEventListener("click", () => {
         currentRoote = ind;
-        updateSliderPositionTwo();
-        updateCircles();
+        updateSliderPosition(rooteImg, currentRoote);
+        updateCircleBtn(mainThreeCicle, currentRoote)
     });
 });
 
-updateSliderPositionTwo();
+
