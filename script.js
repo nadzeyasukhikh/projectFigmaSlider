1ed3a7e40a79a5702b09835109d707ecb741218d
nadzeya <vitserresvit@gmail.com>
2023-10-15 11:41:57 +0300

    new

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const sliderContainer = document.querySelector('.root');
+
 const slides = document.querySelectorAll('.headerText');
-const prevBtn = document.querySelectorAll('.leftRightBtn')[0]; // Первая кнопка
-const nextBtn = document.querySelectorAll('.leftRightBtn')[1]; // Вторая кнопка
+const prevBtn = document.querySelector('.leftBtn');
+const nextBtn = document.querySelector('.rightBtn'); 
 const circleBtn = document.querySelectorAll(".circle");
 let currentSlide = 0;
 circleBtn[0].style.opacity = "1";
@@ -24,6 +24,7 @@ function updateCircleBtn(circleBtn, currentSlide) {
 }
 
 function go_left() {
+    
     if (currentSlide !== 0) {
         currentSlide--;
         updateSliderPosition(slides, currentSlide);
@@ -32,6 +33,7 @@ function go_left() {
 }
 
 function go_right() {
+    
     if (currentSlide < slides.length - 1) {
         currentSlide++;
         updateSliderPosition(slides, currentSlide);
@@ -39,7 +41,7 @@ function go_right() {
     }
 }
 
-prevBtn.addEventListener('click', go_left);
+prevBtn.addEventListener('click',go_left);
 nextBtn.addEventListener('click', go_right);
 
 
@@ -57,27 +59,31 @@ const rooteImg = document.querySelectorAll(".rooteImg");
 const mainThreeLeft = document.querySelector(".mainThreeLeft");
 const mainThreeRight = document.querySelector(".mainThreeRight");
 const mainThreeCicle = document.querySelectorAll(".mainThreeCicle");
-const mainThreeDisplay = document.querySelector(".mainThreeDisplay");
+
 
 let currentRoote = 0;
 mainThreeCicle[0].style.opacity = "1";
 
-
-mainThreeLeft.addEventListener('click', () => {
-    if (currentRoote > 0) {
+function goLeft() {
+    
+    if (currentRoote !== 0) {
         currentRoote--;
         updateSliderPosition(rooteImg, currentRoote);
-        updateCircleBtn(mainThreeCicle, currentRoote)
+        updateCircleBtn(mainThreeCicle, currentRoote);
     }
-});
+}
 
-mainThreeRight.addEventListener('click', () => {
+function goRight() {
+    
     if (currentRoote < rooteImg.length - 1) {
         currentRoote++;
         updateSliderPosition(rooteImg, currentRoote);
-        updateCircleBtn(mainThreeCicle, currentRoote)
+        updateCircleBtn(mainThreeCicle, currentRoote);
     }
-});
+}
+mainThreeLeft.addEventListener('click', goLeft)
+mainThreeRight.addEventListener('click', goRight)
+   
 
 
 
@@ -90,3 +96,41 @@ mainThreeCicle.forEach((btn, ind) => {
 });
 
 
+
+
+// третий слайдер
+
+const rootFourDisplay = document.querySelectorAll(".rootFourDisplay"); // Change to querySelectorAll
+const mainFourLeft = document.querySelector(".mainFourLeft");
+const mainFourRight = document.querySelector(".mainFourRight");
+const mainFourCicle = document.querySelectorAll(".mainFourCicle");
+
+let currentFourRoot = 0;
+mainFourCicle[0].style.opacity = "1";
+
+function goLeftFour() {
+    if (currentFourRoot !== 0) {
+        currentFourRoot--;
+        updateSliderPosition(rootFourDisplay, currentFourRoot);
+        updateCircleBtn(mainFourCicle, currentFourRoot);
+    }
+}
+
+function goRightFour() {
+    if (currentFourRoot < rootFourDisplay.length - 1) { 
+        currentFourRoot++;
+        updateSliderPosition(rootFourDisplay, currentFourRoot);
+        updateCircleBtn(mainFourCicle, currentFourRoot);
+    }
+}
+
+mainFourLeft.addEventListener('click', goLeftFour);
+mainFourRight.addEventListener('click', goRightFour);
+
+mainFourCicle.forEach((btn, ind) => {
+    btn.addEventListener("click", () => {
+        currentFourRoot = ind;
+        updateSliderPosition(rootFourDisplay, currentFourRoot);
+        updateCircleBtn(mainFourCicle, currentFourRoot);
+    });
+});
